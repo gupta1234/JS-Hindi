cda00901ce8d855303065594cb07cc2c98355c8a
Saurav Gupta <67362150+gupta1234@users.noreply.github.com>
2024-07-09 07:22:32 +0000

    datatypes

---
+++ 01_basics/02_DataType.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as new version(use strict apan use kar rahe hai toh ya new version ke jaise hi treat karega)
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3, "Hitesh")  // code readability should be high
+
+// console.log("Hitesh")
+
+let name = "saurav"
+let age = 18
+let isLoggedIn = false 
+
+// number => 2 to power 53
+// bigint 
+// string => " "
+// boolean => true/false 
+// null => standlone value(here null is a type value)
+// undefined => means not assign the value
+// symbol => unique
+
+
+// object
+
+console.log(typeof "saurav");
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
