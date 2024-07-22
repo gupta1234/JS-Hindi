fa90eefdfaf3ebbc4977379b2dea7bce82f85133
Saurav Gupta <67362150+gupta1234@users.noreply.github.com>
2024-07-22 10:32:19 +0000

    IIFE

---
+++ 03_basics/04_life.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+//  (function chai(){
+//     console.log(`DB CONNECTED`)
+// })();    // Semi-colon isliye lagaya hai koi ki pta nhi chalta ki wo kaha pr stop ho raha hai ya hoga....
+
+// ( (name) => {
+//     console.log(`DB CONNECTED TWO ${name}`)
+// } )('saurav')
+
+(function one (){
+    console.log("DB Connect")
+})();
+( () => {
+    console.log(`DB CONNECTED JJJ`);
+} )()
