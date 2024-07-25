ecbfaa9d360b83bc04c6f64f9b3289c55ddf1f1a
Saurav Gupta <67362150+gupta1234@users.noreply.github.com>
2024-07-25 09:54:37 +0000

    Switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }                  // shift+alt+down arrow
+
+// const month = 3
+const month = "March"
+
+switch (month) {
+    case "Januray":
+        console.log("Januray")
+        break;
+    case "February":
+        console.log("February")
+        break;
+    case "March":
+        console.log("March")
+        break;
+    case "April":
+        console.log("April")
+        break;
+
+    default:
+        console.log("Default case");
+        break;
+}
